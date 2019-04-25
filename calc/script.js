aefe5f2b7ebe9abef7d19c7fab0d73f981b69ada
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-04-25 21:19:43 +0000

    organize aoush and ap calc

---
+++ calc/script.js
@@ -0,0 +1,45 @@
+$(function() {
+
+    function show() {
+        if ($(window).scrollTop()) {
+            scroll = true;
+            $(".navbar").addClass('bg-light')
+            if ($(document).width() + 15 >= 992) {
+                $("div.collapse").addClass('BorderBar')
+            }
+        }
+        else {
+            scroll = false;
+            $(".navbar").removeClass('bg-light')
+            $("div.collapse").removeClass('BorderBar')
+        }
+    }
+    
+    // If the scrollbar is moved, fade in navbar background
+    show();
+    $(document).scroll(function() {
+        show();
+    });
+
+    // Fixing the expanded navbar when no scroll event occurs
+    $(".navbar-toggler").click(function() {
+        if ($(this).hasClass("collapsed")) {
+            $(".navbar").addClass('bg-light')
+        }
+    });
+    
+    //collapse the expanded navbar upon clicking a link (for screen size smaller than large 992px)
+    $("a.nav-link").click(function() {
+        $("#navbar").collapse('hide');
+    });
+    
+    //a transition from smaller screen size to larger ones 
+    $(window).resize(function() {
+        if ($(window).width() < 992) {
+            $("div.collapse").removeClass('BorderBar')
+        }
+        else {
+            $("div.collapse").addClass('BorderBar')
+        }
+    });
+});
