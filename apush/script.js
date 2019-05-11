5a6227b51fbbce688291f4b3a4b50dd7f40ed223
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-05-11 19:11:31 +0000

    change colors of main screen and shorten display text

--- apush/script.js
+++ apush/script.js
@@ -1,43 +1,68 @@
-
 $(function() {
 
+    // add on to common.js - show() function, 
+    // if no scroll event, navbar links are white. if scroll bar moves down, nav-links darker color
+    // color swapper with ush-link class defined in apush.css
     function show() {
         if ($(window).scrollTop()) {
-            $(".navbar").addClass('bg-light')
+            //links become dark when scroll
+            $(".nav-link").removeClass("ush-link");
+            $(".navbar-brand").css("color","black");
+            $(".navbar-toggler").removeClass("ush-icon")
         }
         else {
-            $(".navbar").removeClass('bg-light')
+            //links become white when not scroll
+            $(".navbar-toggler").addClass("ush-icon")
+            if ($(".navbar-collapse").hasClass("show") == false){
+                $(".navbar-brand").css("color","white");
+                $(".nav-link").addClass("ush-link")
+            }
         }
     }
-
-    // If the scrollbar is moved, fade in navbar background
+    
+    $(".navbar-toggler").click(function(){
+        if ($("div.navbar-collapse").hasClass("collapse")){
+            $(".navbar-brand").css("color","black")
+            $(".nav-link").removeClass("ush-link")
+        }else{
+            $(".nav-link").addClass("ush-link")
+            $(".navbar-brand").css("color","black !important")
+        }
+    });
+    
+    $(window).resize(function() {
+       show();
+    });
+    
+    //calling the function upon screen finishes loading and when scrolling
     show();
     $(document).scroll(function() {
         show();
     });
+    
+    
+    //NOTE [do not confuse] .pd-* class goes to buttons, #pd-* IDs goes to content
+    $(".btn-dark").click(function(){
 
-    // Fixing the expanded navbar when no scroll event occurs
-    $(".navbar-toggler").click(function() {
-        if ($(this).hasClass("collapsed")) {
-            $(".navbar").addClass('bg-light')
+        //close and swap period content
+        if ($(this).hasClass("s1")){
+            $(".pd1,.pd2,.pd3").removeClass("disabled")
+            $("#pd1, #pd2, #pd3").collapse("hide")
         }
-    });
-
-    //collapse the expanded navbar upon clicking a link (for screen size smaller than large 992px)
-    $("a.nav-link").click(function() {
-        $("#navbar").collapse('hide');
-    });
-
-
-    $('.collapser').click(function() {
-        var disp = $(this).html()
-        var part = disp.substring(0, 4);
-        var rem = disp.substring(5, disp.length);
-        if (part == "Show") {
-            $(this).html("Hide " + rem);
+        else if ($(this).hasClass("s2")){
+            $(".pd4,.pd5,.pd6").removeClass("disabled")
+            $("#pd4, #pd5, #pd6").collapse("hide")
+        }else{
+            $(".pd7,.pd8,.pd9").removeClass("disabled")
+            $("#pd7, #pd8, #pd9").collapse("hide")
+            
+            //close expanded text in periods 7 and 8
+            $("#more7, #more8").collapse("hide")
         }
-        else {
-            $(this).html("Show " + rem);
-        }
+        
+        $(this).addClass("disabled")
+        //however... show one of the three within that set (s1,s2,s3)
+        var str1 = "#" + ($(this).attr("class")).substring(0,4) //take the first 3 letters of class list
+        $(str1).toggleClass("collapse");
     });
 });
