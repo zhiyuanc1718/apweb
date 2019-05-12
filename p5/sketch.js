46dbab63541b9b7e0f35362b2b02955e6901dd3a
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-05-12 02:25:13 +0000

    add text to animation, remove classes

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,15 +1,17 @@
 var canvas;
-var x, y, r, g, w, h;
+var x, y, w, h;
 var shapes = [];
 var particles = [];
 const rate = 100; //rate of Shape spawn
 
+//responsive canvas
 function windowResized() {
     w = $('body').width();
     h = $('body').height();
     resizeCanvas(w, h);
 }
 
+//define all variables and generate falling objects
 function setup() {
     w = $('body').width();
     h = $('body').height();
@@ -18,57 +20,56 @@ function setup() {
     canvas.style("z-index", "-1");
     canvas.style('display', 'block');
     rectMode(CENTER)
-    x = width;
-    y = 4 * height / 5;
-    r = 0;
-    g = 255;
-    lives = 3;
+    x = w;
+    y = 4 * h / 5;
     for (var i = 0; i < 20; i++) {
-        particles.push(new particle(-height / 2, height / 2, 0, width, "white"));
+        particles.push(new particle(-h / 2, h / 2, 0, w));
     }
 }
 
 function Background() {
     background(255, 255, 255);
     fill(0, 0, 0, 230)
-    rect(width / 2, height / 2, width, height)
+    rect(w / 2, h / 2, w, h)
     particleB();
 }
 
-function particle(y_min, y_max, x_min, x_max, str) {
+//white falling
+function particle(y_min, y_max, x_min, x_max) {
     this.x = random(x_min, x_max)
     this.y = random(y_min, y_max)
     this.s = random(0.5, 2)
-    var sp;
+
     if (this.s > 1.5) {
-        sp = random(3.3, 3.4)
+        this.sp = random(3.3, 3.6)
     }
     else if (this.s >= 1 && this.s <= 1.5) {
-        sp = random(3, 3.2)
+        this.sp = random(3, 3.2)
     }
     else {
-        sp = random(2.8, 2.9)
+        this.sp = random(2.5, 2.9)
     }
     this.display = function() {
-        stroke(str)
+        stroke("white")
         ellipse(this.x, this.y, 1, 1)
     };
     this.move = function() {
-        this.y += sp;
+        this.y += this.sp;
     };
 }
 
+//activating falling animation and spawn new objects
 function particleB() {
     if (frameCount % 60 == 0) {
         for (var i = 0; i < 10; i++) {
-            particles.push(new particle(-height / 2, 0, 0, width, "white"));
+            particles.push(new particle(-h / 2, 0, 0, w));
         }
     }
 
     for (var t = 0; t < particles.length; t++) {
         particles[t].display()
         particles[t].move()
-        if (particles[t].y > height) {
+        if (particles[t].y > h) {
             particles.splice(t, 1)
         }
     }
@@ -78,44 +79,14 @@ function draw() {
     Background();
     stroke(0);
     ShapeBehavior();
+    if (windowWidth >= 992) {
+        textAlign(CENTER);
+        fill(255)
+        textSize(20);
+        text("Particle Spawn Rate dS/dt = 0.5/sec", w / 2, (4.5*h)/6)
+        text("Don't understand ? → Learn Calculus", w/2, (5*h)/6)
+    }
 }
-
-
-function character() {
-    particles.push(new particle(y + 17, y + 17, x - 20, x - 20, "orange"));
-    particles.push(new particle(y + 17, y + 17, x - 20, x - 20, "red"));
-    particles.push(new particle(y + 17, y + 17, x + 20, x + 20, "orange"));
-    particles.push(new particle(y + 17, y + 17, x + 20, x + 20, "red"));
-    rectMode(CENTER)
-    strokeWeight(3)
-    fill(0, 0, 255)
-    rect(x, y, 15, 60)
-    rect(x - 22.5, y + 20, 30, 20)
-    rect(x + 22.5, y + 20, 30, 20)
-    fill(255, 0, 0)
-    ellipse(x - 25, y + 20, 10, 10)
-    ellipse(x + 25, y + 20, 10, 10)
-    fill(0)
-    rect(x - 25, y + 2.5, 5, 15)
-    rect(x + 25, y + 2.5, 5, 15)
-}
-
-function bullet() {
-    this.x = x;
-    this.y = y;
-    var bSpeed = 6;
-
-    this.display = function() {
-        rectMode(CENTER)
-        fill(49, 85, 142)
-        rect(this.x, this.y - 50, 10, 30, 20)
-    };
-
-    this.move = function() {
-        this.y -= bSpeed;
-    };
-}
-
 
 function polygon(x, y, radius, sides) {
     var angle = TWO_PI / sides;
@@ -129,15 +100,26 @@ function polygon(x, y, radius, sides) {
 }
 
 function Shape() {
-    this.x = random(45, (3 / 4) * width - 35);
+    this.x = random(45, (3 / 4) * w - 35);
     this.y = 0;
     this.d_y = random(1, 1.5);
     this.d_x = random(-1, 1) * 2;
-    this.sides = floor(random(3, 10));
-    this.rad = floor(random(20,25));
+    this.cir = random(0, 1);
+    this.sides = floor(random(3, 8));
+    this.rad = floor(random(20, 25));
     this.display = function() {
         fill(165, 19, 39)
-        polygon(this.x, this.y, this.rad, this.sides)
+        push();
+        translate(this.x, this.y);
+        rotate(frameCount / 20.0);
+        if (this.cir >= 0.8) {
+            ellipse(0, 0, 1.5 * this.rad, 1.5 * this.rad)
+        }
+        else {
+            polygon(0, 0, this.rad, this.sides)
+        }
+        pop();
+
     };
     this.move = function() {
         this.x += this.d_x
@@ -153,7 +135,8 @@ function ShapeBehavior() {
     for (var i = 0; i < shapes.length; i++) {
         shapes[i].display();
         shapes[i].move();
-        if (shapes[i].x < 30 || shapes[i].x > width - 30) {
+
+        if (shapes[i].x < 30 || shapes[i].x > w - 30) {
             shapes[i].d_x *= -1
         }
     }
