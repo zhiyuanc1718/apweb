60993725f3887d8733b26f4249e76de83f05400a
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-05-12 07:33:20 +0000

    add fonts

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 $(function() {
 
     var num = 0;
-    var quotes = ['"Stay Happy and Love AP"', '"The eyes of the world are upon you"', '"Life is like riding a bicycle. To keep your balance you must keep moving"'];
-    var names = ["Zhiyuan Chen", "Dwight David Eisenhower", "Albert Einstein"];
+    var quotes = ['"Stay Happy and Love AP"', '"The eyes of the world are upon you"', 
+    '"Life is like riding a bicycle. To keep your balance you must keep moving."', 
+    '"I can calculate the motion of heavenly bodies, but not the madness of people."',
+    '"The important thing is not to stop questioning. Curiosity has its own reason for existing."'];
+    var names = ["Zhiyuan Chen", "Dwight David Eisenhower", "Albert Einstein", "Issac Newton", "Albert Einstein"];
 
     changeContent();
     setInterval(function() {
         num++;
-        if (num > 2) {
+        if (num > 4) {
             num = 0;
         }
         changeContent();
-    }, 10000);
+    }, 8000);
 
     function changeContent(){
         $('.quote').html(quotes[num]).css("color","#F1543A");
