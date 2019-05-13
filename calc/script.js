43dcea39c447b3c66f6fcc2c6c18c36101af06cb
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-05-13 00:58:40 +0000

    shortening js & checkover

--- calc/script.js
+++ calc/script.js
@@ -108,49 +108,44 @@ $(function() {
     });
 
     //Modal Body Content 
-    const g1 = '<img id="graph1" src="../img/graph1.png" alt="graph of x+1" style="width:80%">' + '<p>Clearly, the Y value at X = 3 is 4 and the function will approach 4 as X becomes 3.</p>' +
+    const embed = [
+        //Graph 1
+        '<iframe src="https://www.desmos.com/calculator/0j1kagoymb?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' + '<p>Clearly, the Y value at X = 3 is 4 and the function will approach 4 as X becomes 3.</p>' +
         '<p>However, if you look carefully at the equation, the function is undefined at X = 1</p>' +
-        '<p>The graph shows that the function approaches a Y value of 2 for X = 1</p>';
+        '<p>The graph shows that the function approaches a Y value of 2 for X = 1</p>',
 
-    const g2 = '<iframe src="https://www.desmos.com/calculator/kvtixvm0hs?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
+        //Graph 2
+        '<iframe src="https://www.desmos.com/calculator/kvtixvm0hs?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
         '<p>As the graph approaches X = 0 from the <strong><em>LEFT</em></strong>, the graph approaches <em><strong>negative infinity</strong></em></p>' +
         '<p>As the graph approaches X = 0 from the <strong><em>RIGHT</em></strong>, the graph approaches <em><strong>positive infinity</strong></em></p>' +
-        '<p>When the left and right limits do not equal one another, we say that the limit at the X value does not exist. To be considered a limit, the value of the limits cannot be infinity (ironically, infinity has no limit).</p>';
+        '<p>When the left and right limits do not equal one another, we say that the limit at the X value does not exist. To be considered a limit, the value of the limits cannot be infinity (ironically, infinity has no limit).</p>',
 
-    const g3 = '<iframe src="https://www.desmos.com/calculator/cbfetucqim?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
-        '<p>Looking from the left side, the expected y value (limit) is -1 at x = 2. However, looking from the right hand side, the limit is 1.</p>';
+        //Graph 3
+        '<iframe src="https://www.desmos.com/calculator/cbfetucqim?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
+        '<p>Looking from the left side, the expected y value (limit) is -1 at x = 2. However, looking from the right hand side, the limit is 1.</p>',
 
-    const g4 = '<iframe src="https://www.desmos.com/calculator/iqtulixwqj?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
-        '<p>Looking from the right side, the expected y value (limit) is 4 at x = 4. However, looking from the left hand side, the limit is -4.</p>';
+        //Graph 4
+        '<iframe src="https://www.desmos.com/calculator/iqtulixwqj?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
+        '<p>Looking from the right side, the expected y value (limit) is 4 at x = 4. However, looking from the left hand side, the limit is -4.</p>',
+        
+        //Graph 5
+        '<iframe src="https://www.desmos.com/calculator/nxnxg8tx6v?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
+        "<p>What is special about this absolute value graph? Well, it's continous if you are wondering!</p>",
 
-    const g5 = '<iframe src="https://www.desmos.com/calculator/nxnxg8tx6v?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
-        "<p>What is special about this absolute value graph? Well, it's continous if you are wondering!</p>";
+        //Graph 6
+        '<iframe src="https://www.desmos.com/calculator/kjtkxxlwez?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
+        '<p>This graph has a vertical asymptote at x = 0. The graph has a nonremovable discontinuity.</p>',
 
-    const g6 = '<iframe src="https://www.desmos.com/calculator/kjtkxxlwez?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
-        '<p>This graph has a vertical asymptote at x = 0. The graph has a nonremovable discontinuity.</p>';
+        //Graph 7
+        '<iframe src="https://www.desmos.com/calculator/ghgkxjwvkk?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
+        '<p>As you probably wonder, this is indeed a piecewise function.</p>' + '<p>The jump discontinuity occurs at x = 0 where the graph approaches different values from left and right. There are two defined values for f(0).</p>'
+    ];
 
-    const g7 = '<iframe src="https://www.desmos.com/calculator/ghgkxjwvkk?embed" width="90%" height="400px" style="border: 1px solid #ccc" frameborder=0></iframe>' +
-        '<p>As you probably wonder, this is indeed a piecewise function.</p>' + '<p>The jump discontinuity occurs at x = 0 where the graph approaches different values from left and right. There are two defined values for f(0).</p>';
-
-    $(".g1").click(function() {
-        $(".modal-body").html("").append(g1);
+    //graph modal button is clicked, check button secondary class.
+    $(".gm").click(function(){
+        console.log($(this).attr("class"))
+        var randList = $(this).attr("class").split("g");
+        $(".modal-body").html("").append(embed[randList[1]-1]);
     });
-    $(".g2").click(function() {
-        $(".modal-body").html("").append(g2);
-    });
-    $(".g3").click(function() {
-        $(".modal-body").html("").append(g3);
-    });
-    $(".g4").click(function() {
-        $(".modal-body").html("").append(g4);
-    });
-    $(".g5").click(function() {
-        $(".modal-body").html("").append(g5);
-    });
-    $(".g6").click(function() {
-        $(".modal-body").html("").append(g6);
-    });
-    $(".g7").click(function() {
-        $(".modal-body").html("").append(g7);
-    });
+    
 });
