9fcfac4d02b2674836af6b8a92177114edc821ab
Jackson Wu <jacksonw4881@hstat.org>
2019-05-13 01:50:11 +0000

    add quotes

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ $(function() {
 
     //Add Quotes here
     var num = 0;
-    var quotes = ['"Stay Happy and Love AP"', '"The eyes of the world are upon you"',
+    var quotes = ['"Stay Happy and Love AP"', '"Motivation is brief, but discipline is unyielding."',
+        '"Your tower of knowledge will fall if you' + "don't" + 'have a solid base."',
+        '"Sleep can wait. Your homework' + "can't" + '."',
+        '"The eyes of the world are upon you"',
         '"Life is like riding a bicycle. To keep your balance you must keep moving."',
         '"Success is not final , failure is not fatal : it is the courage to continue that counts."',
         '"Go confidently in the direction of your dreams. Live the life you have imagined."',
         '"The important thing is not to stop questioning. Curiosity has its own reason for existing."',
-        "Your tower of knowledge will fall if you don't have a solid base"
     ];
-    var names = ["Zhiyuan Chen", "Dwight David Eisenhower", "Albert Einstein",
-        "Winston Churchill", "Henry David Thoreau", "Albert Einstein", "Yahia Elhag"
+    var names = ["Zhiyuan Chen", "Jackson Wu", "Yahia Elhag", "Kevin Hernandez",
+        "Dwight David Eisenhower", "Albert Einstein",
+        "Winston Churchill", "Henry David Thoreau", "Albert Einstein"
     ];
 
     //JS 
