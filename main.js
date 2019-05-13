af4c70a08d9e59755943b4591f545a8a7adaf934
Jackson Wu <jacksonw4881@hstat.org>
2019-05-13 02:49:01 +0000

    last quote and speed update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(function() {
     //Add Quotes here
     var num = 0;
     var quotes = ['"Stay Happy and Love AP"', '"Motivation is brief, but discipline is unyielding."',
+        '"Complaining about your struggles will only lead you away from progress."',
         '"Your tower of knowledge will fall if you ' + "don't" + ' have a solid base."',
         '"Sleep can wait. Your homework ' + "can't" + '."',
         '"The eyes of the world are upon you"',
@@ -11,12 +12,12 @@ $(function() {
         '"Go confidently in the direction of your dreams. Live the life you have imagined."',
         '"The important thing is not to stop questioning. Curiosity has its own reason for existing."',
     ];
-    var names = ["Zhiyuan Chen", "Jackson Wu", "Yahia Elhag", "Kevin Hernandez",
+    var names = ["Zhiyuan Chen", "Jackson Wu", "Syeda Afroj", "Yahia Elhag", "Kevin Hernandez",
         "Dwight David Eisenhower", "Albert Einstein",
         "Winston Churchill", "Henry David Thoreau", "Albert Einstein"
     ];
     
-    var descript = ["Creator", "Creator", "AP Student", "AP Student"];
+    var descript = ["Creator", "Creator", "AP Student", "AP Student", "AP Student"];
 
     //JS 
     changeContent();
@@ -26,7 +27,7 @@ $(function() {
             num = 0;
         }
         changeContent();
-    }, 8000);
+    }, 5000);
 
     function changeContent() {
         $('.quote').html(quotes[num]).css("color", "#F1543A");
