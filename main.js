8a8b0041c30d950b0797a862b8eb4cc04d5b1df4
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-05-16 13:07:52 +0000

    changing quotes

--- main.js
+++ main.js
@@ -2,22 +2,20 @@ $(function() {
 
     //Add Quotes here
     var num = 0;
-    var quotes = ['"Stay Happy and Love AP"', '"Motivation is brief, but discipline is unyielding."',
-        '"Complaining about your struggles will only lead you away from progress."',
+    var quotes = ['"Success is Overrated"', '"Motivation is brief, but discipline is unyielding."',
         '"Your tower of knowledge will fall if you ' + "don't" + ' have a solid base."',
-        '"Sleep can wait. Your homework ' + "can't" + '."',
         '"The eyes of the world are upon you"',
         '"Life is like riding a bicycle. To keep your balance you must keep moving."',
         '"Success is not final , failure is not fatal : it is the courage to continue that counts."',
         '"Go confidently in the direction of your dreams. Live the life you have imagined."',
         '"The important thing is not to stop questioning. Curiosity has its own reason for existing."',
     ];
-    var names = ["Zhiyuan Chen", "Jackson Wu", "Syeda Afroj", "Yahia Elhag", "Kevin Hernandez",
+    var names = ["Zhiyuan Chen", "Jackson Wu", "Yahia Elhag",
         "Dwight David Eisenhower", "Albert Einstein",
         "Winston Churchill", "Henry David Thoreau", "Albert Einstein"
     ];
     
-    var descript = ["Creator", "Creator", "AP Student", "AP Student", "AP Student"];
+    var descript = ["Creator", "Creator", "AP Student"];
 
     //JS 
     changeContent();
